154698087eb8e456b93affdb9c7db9b49feba439
Nabile Rahmani <nrahmani@diginamic-formation.fr>
2022-06-13 10:29:32 +0200

    TP4.

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class TestMethodeStatic
+{
+
+	public static void main(String[] args)
+	{
+		var string = "12";
+		var integer = Integer.parseInt(string);
+		int a = 4, b = 2;
+		var max = Integer.max(a, b);
+		
+		System.out.println(max);
+	}
+}
