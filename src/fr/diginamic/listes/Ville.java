5009e96dbcc8180b668ee926827db93445484a82
Nabile Rahmani <nrahmani@diginamic-formation.fr>
2022-06-23 10:35:10 +0200

    Maven.

--- src/fr/diginamic/listes/Ville.java
+++ src/fr/diginamic/listes/Ville.java
@@ -1,5 +1,7 @@
 package fr.diginamic.listes;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
 public class Ville implements Comparable<Ville>
 {
 	public String nom;
@@ -23,8 +25,9 @@ public class Ville implements Comparable<Ville>
 		if (obj instanceof Ville)
 		{
 			Ville other = (Ville) obj;
-			
-			return nom == other.nom && nombreHabitants == other.nombreHabitants;
+
+			//return nom == other.nom && nombreHabitants == other.nombreHabitants;
+			return this == other || new EqualsBuilder().append(nom, other.nom).append(nombreHabitants, other.nombreHabitants).isEquals();
 		}
 		else
 			return false;
